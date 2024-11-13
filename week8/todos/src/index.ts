f5334d5df14085882f1f5a144665c254acddf8f2
Your Name <you@example.com>
2024-11-13 10:40:57 -0800

    yesterday work

--- week8/todos/src/index.ts
+++ week8/todos/src/index.ts
@@ -1,7 +1,9 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static'
 
 const app = new Hono()
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
