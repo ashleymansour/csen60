39b28625e139f47a1f142b54ab47130ed0bb2727
Your Name <you@example.com>
2024-11-13 11:28:08 -0800

    add serveStatic and api endpoint for data

--- week8/todos/src/index.ts
+++ week8/todos/src/index.ts
@@ -1,12 +1,31 @@
 import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/serve-static'
 
 const app = new Hono()
 
+//our database
+const todos = [
+{
+  id: 1, 
+  description: "Turn in homework",
+  isComplete: false,
+},
+{
+  id: 2, 
+  description: "Study for test",
+  isComplete: false,
+}
+]
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use("/*", serveStatic({
+   root: "./static",
+}))
+
+app.get('/api/todos', (c) => {
+  console.log("fething and returning todos")
+  //database call to fetch todos
+  return c.json(todos)
 })
 
 const port = 3000
